7f0a44cf54765163c6b380467035440592d30842
Tomasz Andrzejak <andreiltd@gmail.com>
2023-11-30 19:45:52 +0100

    server: Ignore NotConnected errors on shutdown

--- src/proto/connection.rs
+++ src/proto/connection.rs
@@ -289,7 +289,18 @@ where
                 State::Closing(reason, initiator) => {
                     tracing::trace!("connection closing after flush");
                     // Flush/shutdown the codec
-                    ready!(self.codec.shutdown(cx))?;
+                    if let Err(e) = ready!(self.codec.shutdown(cx)) {
+                        // If the error kind is NotConnected, ignore it, since
+                        // it just means the connection is already shutdown.
+                        if e.kind() != io::ErrorKind::NotConnected {
+                            return Poll::Ready(Err(e.into()));
+                        } else {
+                            tracing::trace!(
+                                "ignoring NotConnected error \
+                                (the connection has already closed)"
+                           );
+                       }
+                    }
 
                     // Transition the state to error
                     self.inner.state = State::Closed(reason, initiator);
